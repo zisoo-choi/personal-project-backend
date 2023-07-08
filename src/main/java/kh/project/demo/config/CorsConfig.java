ecc7acf34c2deada5b64ff2992f8214bee87f930
zisoo <zisoo_3@naver.com>
2023-07-09 00:39:55 +0900

    init: [최지수] front CD 세팅을 위한 CorsConfig [FCLP-2]

---
+++ src/main/java/kh/project/demo/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package kh.project.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://127.0.0.1:8080/", // vue는 허용하겠다는 소리
+                        "http://localhost:8080/",
+                        "http://52.78.6.90"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}

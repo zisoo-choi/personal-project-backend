0330117a002ac082ca4006622abdac2e9dd29c64
zisoo <zisoo_3@naver.com>
2023-07-27 17:32:56 +0900

    remove: [최지수] 불필요한 레포지토리 삭제

--- src/main/java/kh/project/demo/library/libraryService/repository/LibraryRepository.java
+++
@@ -1,8 +0,0 @@
-package kh.project.demo.library.libraryService.repository;
-
-import kh.project.demo.library.libraryService.entity.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibraryRepository extends JpaRepository<Rental, Long> {
-
-}
